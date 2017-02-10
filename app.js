53e86e93c8d7778f6c8cea4434b1cf9bc10a5156
leminhquan <leminhquan@luvina.net>
2017-02-10 14:13:17 +0700

    sample private-web-chat

--- app.js
+++ app.js
@@ -1,46 +1,25 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+// This is the main file of our chat app. It initializes a new 
+// express.js instance, requires the config and routes files
+// and listens on a port. Start the application by running
+// 'node app.js' in your terminal
 
-var index = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+var express = require('express'),
+	app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+// This is needed if the app is run on heroku:
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var port = process.env.PORT || 3000;
 
-app.use('/', index);
-app.use('/users', users);
+// Initialize a new socket.io object. It is bound to 
+// the express app, which allows them to coexist.
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+var io = require('socket.io').listen(app.listen(port));
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// Require the configuration and the routes files, and pass
+// the app and io as arguments to the returned functions.
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+require('./config')(app, io);
+require('./routes')(app, io);
 
-module.exports = app;
+console.log('Your application is running on http://localhost:' + port);
